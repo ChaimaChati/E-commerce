76f25ab58939d4700fe4865a0dc0147450d84fd7
Chaima Chati <45006780+ChaimaChati@users.noreply.github.com>
2020-04-26 21:43:31 +0000

    Update checkout.php

--- HCH_Furniture/checkout.php
+++ HCH_Furniture/checkout.php
@@ -9,10 +9,44 @@ else {
     $user = $_SESSION['user_login'];
     $sql="SELECT * FROM user WHERE id='$user'";
     $result= $conn->query($sql);
-
         $get_user_email =$result->fetch(PDO::FETCH_ASSOC);
             $uname_db = $get_user_email['firstName'];
 }
+
+if (isset($_POST['submit'])) {
+//declere veriable
+$fname=$_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$country = $_POST['country'];
+$town = $_POST['town'];
+$zipcode = $_POST['zipcode'];
+$comment = $_POST['comment'];
+		
+		// Check if livre code already exists
+		
+	
+		if (strlen($_POST['comment']) >50 ) {
+						
+						
+						$sql="INSERT INTO comment (fName,lName,email,country,town,zipcode,comment) VALUES ('$_POST[fname]','$_POST[lname]','$_POST[email]','$_POST[country]','$_POST[town]','$_POST[zipcode]','$_POST[comment]')";
+								$result = $conn->query($sql);
+								
+						
+		}else {
+						throw new Exception('comment should be above 50 characters');
+					}
+
+	
+}
+
+
+?>
+
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -128,41 +162,34 @@ else {
                             <form action="#" method="post">
                                 <div class="row">
                                     <div class="col-md-6 mb-3">
-                                        <input type="text" class="form-control" id="first_name" value="" placeholder="First Name" required>
+                                        <input type="text" class="form-control" id="first_name" name="fname" value="" placeholder="First Name" required>
                                     </div>
                                     <div class="col-md-6 mb-3">
-                                        <input type="text" class="form-control" id="last_name" value="" placeholder="Last Name" required>
+                                        <input type="text" class="form-control" id="last_name" name="lname" value="" placeholder="Last Name" required>
                                     </div>
                                     <div class="col-12 mb-3">
-                                        <input type="email" class="form-control" id="email" placeholder="Email" value="">
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="">
                                     </div>
                                     <div class="col-12 mb-3">
-                                        <select class="w-100" id="country">
-                                        <option value="usa">United States</option>
-                                        <option value="uk">United Kingdom</option>
-                                        <option value="ger">Germany</option>
-                                        <option value="fra">France</option>
-                                        <option value="ind">India</option>
-                                        <option value="aus">Australia</option>
-                                        <option value="bra">Brazil</option>
-                                        <option value="cana">Canada</option>
-                                    </select>
+                                        <input type="text" class="form-control" id="city" name="country" placeholder="country" value="">
                                     </div>
                                     
                                     <div class="col-12 mb-3">
-                                        <input type="text" class="form-control" id="city" placeholder="Town" value="">
+                                        <input type="text" class="form-control" id="city" name="town" placeholder="Town" value="">
                                     </div>
                                     <div class="col-md-6 mb-3">
-                                        <input type="text" class="form-control" id="zipCode" placeholder="Zip Code" value="">
+                                        <input type="text" class="form-control" id="zipCode" name="zipcode" placeholder="Zip Code" value="">
                                     </div>
                                     
                                     <div class="col-12 mb-3">
-                                        <textarea name="comment" class="form-control w-100" id="comment" cols="30" rows="10" placeholder="Leave a comment about your order"></textarea>
+                                        <textarea name="comment" class="form-control w-100" name="comment" id="comment" cols="30" rows="10" placeholder="Leave a comment about your order" required></textarea>
                                     </div>
                                    
                                     <div class="col-md-4 mb-3">
                                      <div class="cart-btn mt-100">
-                                        <a href="#" class="btn amado-btn w-100">Submit</a>
+                                         <button type="submit" name="submit" value="submit" class="btn amado-btn w-100">submit
+                                        </button>
+                                       
                                     </div>
                                         
                                     </div>
